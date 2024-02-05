75010a215ddf3955a0c3d06ca54905d62904c74e
Fernando Silva <fernandosilvot@gmail.com>
2024-02-05 12:58:57 -0300

    version pricipios de año

---
+++ assets/js/script_2.js
@@ -0,0 +1,25 @@
+var typed_name = new Typed('#name', {
+    strings: ['Fernando Silva T'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_rol = new Typed('#rol', {
+    strings: ['Estudiante'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_email = new Typed('#email', {
+    strings: ['fernandosilvot@gmail.com'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_linkedin = new Typed('#linkedin', {
+    strings: ['Fernando Silva Trincado'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_github = new Typed('#github', {
+    strings: ['fernandosilvot'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
